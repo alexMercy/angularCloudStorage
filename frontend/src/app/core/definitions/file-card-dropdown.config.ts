4c578deda54fe86b2c7f4a9bc6f7e63f27d7dbe0
alexMercy <68870653+alexMercy@users.noreply.github.com>
2023-06-14 17:25:10 +0300

    good realization of dynamic dropdown

--- frontend/src/app/core/definitions/file-card-dropdown.config.ts
+++ frontend/src/app/core/definitions/file-card-dropdown.config.ts
@@ -7,8 +7,8 @@ interface FileCardMenuItem {
 export const fileCardContextMenuConfig: Array<FileCardMenuItem> = [
   {title: "1st item", click: ()=>console.log("1 clicked")},
   {title: "2nd item", click: ()=>console.log("2 clicked")},
+  {title: "3rd item", click: ()=>console.log("3 clicked")},
   {title: "Sub menu", children: [
       {title: "4th item", click: ()=>console.log("4 clicked")},
-    ]},
-  {title: "3rd item", click: ()=>console.log("3 clicked")}
+    ]}
 ]
